f6f7bbdf2b6fb1e3e9c68a9f486be99cf8f59173
Nirupam <nirupamatiitd@gmail.com>
2025-12-04 11:49:03 +0530

    first commit

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import Message from './Message'
+import Message from "./Message";
 
 const ChatContainer = ({
   messages,
@@ -7,24 +7,35 @@ const ChatContainer = ({
   messagesContainerRef,
 }) => {
   return (
-    <div className={`chat-container ${isVisible ? 'show' : ''}`} id="chatContainer">
+    <div
+      className={`chat-container ${isVisible ? "show" : ""}`}
+      id="chatContainer"
+    >
       <div className="chat-header" onClick={onMinimize}>
         <div className="header-content">
           <div id="logoText">Ask SAM your questions!</div>
           <div id="subText">AI Engine Powered by QRaie</div>
         </div>
       </div>
-      <div className="messages" ref={messagesContainerRef} id="messagesContainer">
+      <div
+        className="messages"
+        ref={messagesContainerRef}
+        id="messagesContainer"
+      >
         {messages.map((message) => (
           <Message key={message.id} message={message} />
         ))}
       </div>
-      <a href="#" target="_blank" className="demo-button" rel="noreferrer">
+      <a
+        href="https://delaware.gov/contact/#readspeaker_button1"
+        target="_blank"
+        className="demo-button"
+        rel="noreferrer"
+      >
         Request a Demo
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default ChatContainer
-
+export default ChatContainer;
